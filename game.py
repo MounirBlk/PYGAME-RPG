4ac9173d413dd34533a61a63067ef5e299d1a601
MounirBlk <mounir.ballouk@gmail.com>
2021-08-25 20:51:27 +0200

    🚀 RELEASE: PART 3 - PROJECTILES

--- game.py
+++ game.py
@@ -5,12 +5,10 @@ from player import Player
 class Game:
     def __init__(self):
         pygame.init()
-        width = 1080
-        height = 720
-        self.screen = pygame.display.set_mode((width, height)) #Creer la fenetre du jeu
+        self.screen = pygame.display.set_mode((1080, 720)) #Creer la fenetre du jeu
         pygame.display.set_caption("PyGame - Comet fall")
         self.background = pygame.image.load('assets/bg.jpg')
-        self.player = Player()
+        self.player = Player(self.screen)
         self.pressed = {}
     def walls(self, direction):
         if direction == 'right':
@@ -20,7 +18,7 @@ class Game:
         
     def commandes(self):
         if self.pressed.get(pygame.K_RIGHT) and self.pressed.get(pygame.K_LEFT):
-            print('POWER')
+            self.player.health = self.player.health + 1 if self.player.health < self.player.max_health else self.player.health
         elif self.pressed.get(pygame.K_RIGHT) and self.walls('right'):
             self.player.move('right')
         elif self.pressed.get(pygame.K_LEFT) and self.walls('left'):
@@ -29,6 +27,9 @@ class Game:
     def initialisation(self):
         self.screen.blit(self.background, (0, -200)) #arriere plan
         self.screen.blit(self.player.image, self.player.rect)#appliquer l'image du player
+        for projectile in self.player.all_projectiles:
+            projectile.move()
+        self.player.all_projectiles.draw(self.screen) #appliquer l'ensemble des images du groupe de projectiles
         self.commandes()
         pygame.display.flip() #mettre a jour l'ecran
         
@@ -40,9 +41,12 @@ class Game:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
-                elif event.type == pygame.KEYDOWN:
+                elif event.type == pygame.KEYDOWN: #touche appuyé
                     self.pressed[event.key] = True
-                elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_SPACE: 
+                        self.player.launch_projectile() # lancement du projectile
+                    
+                elif event.type == pygame.KEYUP: #touche non appuyé
                     self.pressed[event.key] = False
 
             clock.tick(60) #SET FPS
--- player.py
+++ player.py
@@ -1,18 +1,24 @@
 #!/usr/bin/env python
 import pygame 
 from pygame import * #a retirer
+from projectile import Projectile#a retirer
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, screen):
         super().__init__()
+        self.screen = screen
         self.health = 100
         self.max_health = 100
         self.attack = 10
         self.speed = 10 #pixel
+        self.all_projectiles = pygame.sprite.Group()
         self.image = pygame.image.load('assets/player.png')
         self.rect = self.image.get_rect()
         self.rect.x = 200
         self.rect.y = 500
+        
+    def launch_projectile(self):
+        self.all_projectiles.add(Projectile(self))
         
     def move(self, direction):
         self.facing = direction
